455de98a6de3fe22415796c0b1caea6951bd8bb3
Andressa Constantino <86380516+Andressalconstantino@users.noreply.github.com>
2021-10-30 01:14:51 -0300

    Create Beecrowd-1585.cpp

---
+++ Beecrowd-1585.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int ct; cin >> ct;
+    for (int i=0; i<ct;i++){
+        int x, y; cin >> x >> y;
+        int r = (x*y)/2;
+        cout << r << " cm2" << '\n';
+    }
+}
